c4df93ff6e12d47bc1dd52c0b4fcb251c1b642a5
Jason Freeberg <jafreebe@microsoft.com>
2021-06-28 21:09:37 -0700

    Create hello.sh

---
+++ week4/hello-docker/hello.sh
@@ -0,0 +1,11 @@
+echo 'Hello from Docker! This message was printed from the Docker container.'
+
+echo "
+To generate this message, Docker took the following steps:
+ 1. The Docker client contacted the Docker daemon.
+ 2. The Docker daemon built a new Docker image based on the Dockerfile.
+ 3. The Docker daemon created a new container from that image which runs the
+    executable that produces the output you are currently reading.
+ 4. The Docker daemon streamed that output to the Docker client, which sent it
+    to your terminal.
+"
